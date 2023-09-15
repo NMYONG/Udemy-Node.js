9cc04226664c67fe16f25d82a95e13df224f8d26
정남용 <jny5360@gmail.com>
2023-09-15 17:08:43 +0900

    230915

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-const adminRoutes = require("./routes/admin");
+const amdinData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", amdinData.routes);
 app.use(shopRoutes);
 
 // status를 호출해 상태 코드를 설정
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,12 @@
 const path = require("path");
 const express = require("express");
 const rooDir = require("../util/path");
+const adminData = require("./admin");
 
 const router = express.Router(); // express.Router을 호출해서 router객체 생성
 
 router.get("/", (req, res, next) => {
+  console.log("shop.js", adminData.products);
   // send.File 파일 전송하기
   res.sendFile(path.join(rooDir, "views", "shop.html"));
 });
