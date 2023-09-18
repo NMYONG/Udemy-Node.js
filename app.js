277893ff9271692809866a064ea53177440eb5b6
정남용 <jny5360@gmail.com>
2023-09-18 17:20:35 +0900

    230918 pug layoyts

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ app.use(shopRoutes);
 
 // status를 호출해 상태 코드를 설정
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404");
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -6,9 +6,11 @@ const adminData = require("./admin");
 const router = express.Router(); // express.Router을 호출해서 router객체 생성
 
 router.get("/", (req, res, next) => {
-  console.log("shop.js", adminData.products);
-  // send.File 파일 전송하기
-  res.sendFile(path.join(rooDir, "views", "shop.html"));
+  // console.log("shop.js", adminData.products);
+  // // send.File 파일 전송하기
+  // res.sendFile(path.join(rooDir, "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", { prods: products, docTitle: "Shop" }); // 하나 이상의 필드를 전달할 수 있다.
 });
 
 module.exports = router;
