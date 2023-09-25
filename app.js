817f78f788aebba4eed6bbe98ad1e810f7f63951
정남용 <jny5360@gmail.com>
2023-09-25 17:43:17 +0900

    230925 MVC(모델 뷰 컨트롤러)

--- app.js
+++ app.js
@@ -2,26 +2,24 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const expressHbs = require("express-handlebars");
+
+const errorController = require("./controllers/error");
 
 const app = express();
 
 app.set("view engine", "ejs"); // Express 앱을 생성하여 app 상수에 저장하면 전체 구성 값을 설정할 수 있다.
 app.set("views", "views"); // Express에게 view를 찾을 지점을 알려줌
 
-const amdinData = require("./routes/admin");
+const amdinRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const { engine } = require("express-handlebars");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", amdinData.routes);
+app.use("/admin", amdinRoutes);
 app.use(shopRoutes);
 
 // status를 호출해 상태 코드를 설정
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
---
+++ controllers/error.js
@@ -0,0 +1,4 @@
+// return 404 page
+exports.get404 = (req, res, next) => {
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
+};
