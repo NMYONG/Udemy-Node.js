8025f89c5878ed45dc1c69cfec36f3ed6fa9e5fb
정남용 <jny5360@gmail.com>
2023-09-27 16:19:39 +0900

    230927 MVC 완료

--- controllers/products.js
+++ controllers/products.js
@@ -1,29 +1,34 @@
-const Product = require("../models/product");
+const Product = require("../models/product"); // Product 모델을 가져옵니다.
 
+// "Add Product" 페이지를 렌더링하는 라우트 핸들러
 exports.getAddProduct = (req, res, next) => {
   res.render("add-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true,
+    pageTitle: "Add Product", // 페이지 제목
+    path: "/admin/add-product", // 현재 경로
+    formsCSS: true, // CSS 스타일을 적용하기 위한 플래그
+    productCSS: true, // CSS 스타일을 적용하기 위한 플래그
+    activeAddProduct: true, // 현재 메뉴 항목을 활성화하기 위한 플래그
   });
 };
 
+// "Add Product" 폼에서 제품 정보를 받아와 데이터베이스에 저장하는 라우트 핸들러
 exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title);
-  product.save();
-  res.redirect("/");
+  const product = new Product(req.body.title); // 요청 본문에서 제품 제목을 가져와 Product 모델을 사용해 인스턴스를 생성합니다.
+  product.save(); // Product 모델의 save 메서드를 사용하여 제품 정보를 저장합니다.
+  res.redirect("/"); // 홈 페이지로 리다이렉트합니다.
 };
 
+// "Shop" 페이지를 렌더링하고 모든 제품 정보를 표시하는 라우트 핸들러
 exports.getProduct = (req, res, next) => {
-  const products = Product.fetchAll();
-  res.render("shop", {
-    prods: products,
-    pageTitle: "Shop",
-    path: "/",
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true,
+  // Product 모델의 fetchAll 메서드를 사용하여 모든 제품 정보를 가져옵니다.
+  Product.fetchAll((products) => {
+    res.render("shop", {
+      prods: products, // 제품 정보를 템플릿에 전달
+      pageTitle: "Shop", // 페이지 제목
+      path: "/", // 현재 경로
+      hasProducts: products.length > 0, // 제품이 있는지 여부를 확인하는 플래그
+      activeShop: true, // 현재 메뉴 항목을 활성화하기 위한 플래그
+      productCSS: true, // CSS 스타일을 적용하기 위한 플래그
+    });
   });
 };
--- models/product.js
+++ models/product.js
@@ -1,40 +1,45 @@
 const fs = require("fs");
 const path = require("path");
 
+// 파일 경로를 생성합니다.
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
+
+// 파일에서 제품 목록을 읽어오는 함수
+const getProductsFromFile = (cb) => {
+  // products.json 파일을 읽어옵니다.
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      // 오류 발생 시 빈 배열을 전달합니다.
+      cb([]);
+    } else {
+      // 파일 내용을 JSON으로 파싱하여 콜백 함수에 전달합니다.
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class Product {
   constructor(t) {
+    // 생성자: 제품의 제목을 받아 인스턴스 변수에 할당합니다.
     this.title = t;
   }
 
   save() {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
-      products.push(this);
+    // 현재 제품을 파일에 저장합니다.
+    getProductsFromFile((products) => {
+      products.push(this); // 현재 제품을 제품 목록에 추가합니다.
       fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
+        console.log(err); // 오류가 있을 경우 오류를 출력합니다.
       });
     });
   }
 
-  static fetchAll() {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        return [];
-      }
-      return JSON.parse(fileContent);
-    });
+  static fetchAll(cb) {
+    // 모든 제품을 가져오는 정적 메서드
+    getProductsFromFile(cb);
   }
 };
